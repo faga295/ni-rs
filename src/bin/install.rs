eaeb2d728e0e9e5cb181cfb6484c7edb6f034e80
faga <lzc295592@163.com>
2023-02-05 03:41:25 +0800

    chore: resolve args

--- src/bin/install.rs
+++ src/bin/install.rs
@@ -4,20 +4,19 @@ use std::{env, error::Error, process::Command};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = env::args().collect();
-    let options = resolve_args(&args);
+    let has_package = resolve_args(&args);
 
     let manager = detect_package_manager();
     let mut install_arg = "install";
-    if let Some(_) = options.get("value") {
-        if manager.eq("yarn") || manager.eq("pnpm") {
-            install_arg = "add"
-        }
+    if has_package && (manager.eq("yarn") || manager.eq("pnpm")) {
+        install_arg = "add"
     }
     let mut args_str = String::new();
     for arg in &args[1..] {
-        args_str += arg
+        args_str += " ";
+        args_str += arg;
     }
-    println!("> {} {} {}", manager, install_arg, args_str);
+    println!("> {} {}{}", manager, install_arg, args_str);
     let mut install = Command::new(manager);
     install.arg(install_arg).args(&args[1..]).status()?;
     Ok(())
--- src/utils/args.rs
+++ src/utils/args.rs
@@ -1,20 +1,8 @@
-use std::collections::HashMap;
-
-pub fn resolve_args(args: &Vec<String>) -> HashMap<String, Vec<String>> {
-    let mut options: HashMap<String, Vec<String>> = HashMap::from([
-        (String::from("name"), vec![]),
-        (String::from("value"), vec![]),
-    ]);
+pub fn resolve_args(args: &Vec<String>) -> bool {
     for arg in args {
-        if arg.starts_with('-') {
-            if let Some(vec) = options.get_mut("name") {
-                vec.push(arg.to_string())
-            }
-        } else {
-            if let Some(vec) = options.get_mut("value") {
-                vec.push(arg.to_string())
-            }
+        if !arg.starts_with('-') {
+            return true;
         }
     }
-    options
+    false
 }
